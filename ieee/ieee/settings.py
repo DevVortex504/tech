a52b1b7907efa597cbe1e1722e3b920a535ba907
Trishit Debsharma <trishit_debsharma@Trishits-MacBook-Air.local>
2024-04-26 22:17:01 +0530

    changed database and modified for hosting

--- ieee/ieee/settings.py
+++ ieee/ieee/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,14 +75,24 @@ AUTH_USER_MODEL = "recipe.User"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+'''
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER":"postgres",
+        "PASSWORD":"FHHzgiRjmhoXlFhfkDOcKRgIWMuZrwIg",
+        "HOST":"monorail.proxy.rlwy.net",
+        "PORT":"54803",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -119,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
